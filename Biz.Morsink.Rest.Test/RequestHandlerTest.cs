5348a1bfaf88684a5110db0a8f17e2e9a2341d8f
Joost W. Morsink <morsink@gmail.com>
2017-07-30 13:45:37 +0200

    Test for Request handler

---
+++ Biz.Morsink.Rest.Test/RequestHandlerTest.cs
@@ -0,0 +1,31 @@
+﻿using Biz.Morsink.Identity;
+using Biz.Morsink.Rest.Test.Helpers;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Biz.Morsink.Rest.Test
+{
+    [TestClass]
+    public class RequestHandlerTest
+    {
+        [TestMethod]
+        public async Task RequestHandler_HappyGet()
+        {
+            var testrepo = new TestGetRepo();
+            var handler = new RestRequestHandler(ty => ty == typeof(Person) ? testrepo : null);
+            var req = new RestRequest("GET", FreeIdentity<Person>.Create(1), Enumerable.Empty<(string, string)>(), ty => new object());
+            var response = await handler.HandleRequest(req) as RestResponse<Person>;
+            Assert.IsNotNull(response);
+            Assert.IsTrue(response.IsSuccess);
+
+            var val = response.Value.AsSuccess().RestValue.Value;
+            Assert.AreEqual("Joost", val.FirstName);
+            Assert.AreEqual("Morsink", val.LastName);
+            Assert.AreEqual(37, val.Age);
+        }
+    }
+}
