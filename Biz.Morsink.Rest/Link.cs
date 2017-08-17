5fe315067acccded2a6b85588f0df012a8d81391
Joost W. Morsink <morsink@gmail.com>
2017-08-17 10:39:20 +0200

    Parameterized Links

--- Biz.Morsink.Rest/Link.cs
+++ Biz.Morsink.Rest/Link.cs
@@ -7,34 +7,35 @@ namespace Biz.Morsink.Rest
 {
     public class Link
     {
-        public static Link Create(string relType, IIdentity target, Type capability = null)
-            => new Link(relType, target, capability);
-        public static Link<T> Create<T>(string relType, IIdentity<T> target, Type capability = null)
+        public static Link Create(string relType, IIdentity target, object parameters = null, Type capability = null)
+            => new Link(relType, target, parameters, capability);
+        public static Link<T> Create<T>(string relType, IIdentity<T> target, object parameters = null, Type capability = null)
             where T : class
-            => new Link<T>(relType, target, capability);
-        internal Link(string relType, IIdentity target, Type capability)
+            => new Link<T>(relType, target, parameters, capability);
+        internal Link(string relType, IIdentity target, object parameters, Type capability)
         {
             RelType = relType;
             Target = target;
-            Capability = capability ?? typeof(IRestGet<,>).MakeGenericType(target.ForType, typeof(NoParameters));
+            Parameters = parameters;
+            Capability = capability ?? typeof(IRestGet<,>).MakeGenericType(target.ForType, parameters?.GetType() ?? typeof(NoParameters));
         }
         public string RelType { get; }
         public IIdentity Target { get; }
+        public object Parameters { get; }
         public Type Capability { get; }
         public Link<T> As<T>()
             where T : class
         {
-            var res = this as Link<T>;
-            if (res != null)
+            if (this is Link<T> res)
                 return res;
             var id = Target as IIdentity<T>;
-            return id == null ? null : new Link<T>(RelType, id, Capability);
+            return id == null ? null : new Link<T>(RelType, id, Parameters, Capability);
         }
     }
     public class Link<T> : Link
         where T : class
     {
-        internal Link(string relType, IIdentity<T> target, Type capability) : base(relType, target, capability ?? typeof(IRestGet<T, NoParameters>))
+        internal Link(string relType, IIdentity<T> target, object parameters, Type capability) : base(relType, target, parameters, capability ?? typeof(IRestGet<T, NoParameters>))
         { }
         public new IIdentity<T> Target => (IIdentity<T>)base.Target;
     }
