d15e6b7f939e90512b467ea8b6b3b8c8ed4341f9
Joost W. Morsink <morsink@gmail.com>
2017-08-19 14:13:21 +0200

    Rest pipeline

---
+++ Biz.Morsink.Rest/IRestRequestHandlerBuilder.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Biz.Morsink.Rest
+{
+    public interface IRestRequestHandlerBuilder
+    {
+        IRestRequestHandlerBuilder Use(Func<IRestRequestHandler, IRestRequestHandler> handler);
+        IRestRequestHandler Run(Func<IRestRequestHandler> handler);
+    }
+    public static class RestRequestHandlerBuilderExt {
+        public static IRestRequestHandlerBuilder Use(this IRestRequestHandlerBuilder builder, Func<IRestRequestHandler, Func<RestRequest, ValueTask<RestResponse>>> handler)
+            => builder.Use(next => new FunctionalRestRequestHandler(handler(next)));
+        private class FunctionalRestRequestHandler : IRestRequestHandler
+        {
+            private readonly Func<RestRequest, ValueTask<RestResponse>> f;
+
+            public FunctionalRestRequestHandler(Func<RestRequest, ValueTask<RestResponse>> f)
+            {
+                this.f = f;
+            }
+
+            public ValueTask<RestResponse> HandleRequest(RestRequest request)
+                => f(request);
+        }
+        public static IRestRequestHandlerBuilder Use<T>(this IRestRequestHandlerBuilder builder, IServiceLocator locator)
+            where T : IRestRequestHandler
+            =>  builder.Use(next =>
+                {
+                    var ctor = typeof(T).GetTypeInfo().DeclaredConstructors.First();
+                    var parameters = ctor.GetParameters().Select(p => p.ParameterType == typeof(IRestRequestHandler) ? next : locator.ResolveRequired(p.ParameterType)).ToArray();
+                    return (IRestRequestHandler)Activator.CreateInstance(typeof(T), parameters);
+                });
+        
+    }
+}
---
+++ Biz.Morsink.Rest/RestRequestHandlerBuilder.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Immutable;
+
+namespace Biz.Morsink.Rest
+{
+    public class RestRequestHandlerBuilder : IRestRequestHandlerBuilder
+    {
+        public static IRestRequestHandlerBuilder Create()
+            => new RestRequestHandlerBuilder { handlers = ImmutableList<Func<IRestRequestHandler, IRestRequestHandler>>.Empty };
+        private ImmutableList<Func<IRestRequestHandler, IRestRequestHandler>> handlers;
+        private RestRequestHandlerBuilder() { } 
+        
+        public IRestRequestHandlerBuilder Use(Func<IRestRequestHandler, IRestRequestHandler> handler)
+        {
+            return new RestRequestHandlerBuilder { handlers = handlers.Add(handler) };
+        }
+        public IRestRequestHandler Run(Func<IRestRequestHandler> handler)
+        {
+            var result = handler();
+            foreach (var h in handlers.Reverse())
+                result = h(result);
+            return result;
+        }
+    }
+}
