547235ab02acfa8756558ac31e2f2cfe5e685196
Joost W. Morsink <morsink@gmail.com>
2017-10-25 15:54:28 +0200

    Attribute based Rest Identity Provider

--- Biz.Morsink.Rest.AspNetCore/Identity/AttributeBasedRestIdentityProvider.cs
+++ Biz.Morsink.Rest.AspNetCore/Identity/AttributeBasedRestIdentityProvider.cs
@@ -1,14 +1,34 @@
-﻿using System;
+﻿using Microsoft.Extensions.DependencyInjection;
+using System;
 using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
 using System.Text;
 
 namespace Biz.Morsink.Rest.AspNetCore.Identity
 {
     class AttributeBasedRestIdentityProvider : RestIdentityProvider
     {
-        public AttributeBasedRestIdentityProvider(IEnumerable<(RestPathAttribute, Type)> attributes) : base() {
-            foreach(var (attr,type) in attributes)
-                BuildEntry(attr.ComponentTypes).WithPathAndQueryType(attr.Path, attr.WildcardType).Add();
+        public AttributeBasedRestIdentityProvider() : base()
+        {
+
         }
+
+        public void Initialize(IEnumerable<(RestPathAttribute, Type)> attributes)
+        {
+            foreach (var (attr, type) in attributes)
+            {
+                if (attr.WildcardType != null)
+                    BuildEntry(attr.ComponentTypes).WithPathAndQueryType(attr.Path, attr.WildcardType).Add();
+                else
+                    BuildEntry(attr.ComponentTypes).WithPath(attr.Path).Add();
+            }
+        }
+        public void Initialize(IServiceCollection serviceCollection)
+            => Initialize(from desc in serviceCollection
+                          where typeof(IRestRepository).IsAssignableFrom(desc.ServiceType)
+                          from attr in desc.ImplementationType.GetTypeInfo().GetCustomAttributes<RestPathAttribute>()
+                          select (attr, desc.ImplementationType));
+
     }
 }
