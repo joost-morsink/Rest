3edd37628e8cdf0fe4bfc70596663693a7875023
Joost W. Morsink <morsink@gmail.com>
2017-10-25 14:43:08 +0200

    Definition of RestPathAttribute and corresponding Identity Provider.

---
+++ Biz.Morsink.Rest.AspNetCore/Identity/AttributeBasedRestIdentityProvider.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Biz.Morsink.Rest.AspNetCore.Identity
+{
+    class AttributeBasedRestIdentityProvider : RestIdentityProvider
+    {
+        public AttributeBasedRestIdentityProvider(IEnumerable<(RestPathAttribute, Type)> attributes) : base() {
+            foreach(var (attr,type) in attributes)
+                BuildEntry(attr.ComponentTypes).WithPathAndQueryType(attr.Path, attr.WildcardType).Add();
+        }
+    }
+}
---
+++ Biz.Morsink.Rest.AspNetCore/Identity/RestPathAttribute.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Biz.Morsink.Rest.AspNetCore.Identity
+{
+    [AttributeUsage(AttributeTargets.Class)]
+    public class RestPathAttribute : Attribute
+    {
+        public RestPathAttribute(string path, Type[] componentTypes, Type wildcardType = null)
+        {
+            Path = path;
+            ComponentTypes = componentTypes;
+            WildcardType = wildcardType;
+        }
+        public string Path { get; set; }
+        public Type[] ComponentTypes { get; set; }
+        public Type WildcardType { get; set; }
+    }
+}
