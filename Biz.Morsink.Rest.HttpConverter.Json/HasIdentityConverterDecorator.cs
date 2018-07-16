373bb635d8370e665a75b9c3e0b63b3b5cef04b3
Joost W. Morsink <morsink@gmail.com>
2018-07-16 21:57:19 +0200

    Work in progress on DefaultJsonConverter (2 failing tests)

---
+++ Biz.Morsink.Rest.HttpConverter.Json/HasIdentityConverterDecorator.cs
@@ -0,0 +1,41 @@
+﻿using Biz.Morsink.Identity;
+using Biz.Morsink.Rest.AspNetCore;
+using Biz.Morsink.Rest.Utils;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Biz.Morsink.Rest.HttpConverter
+{
+    public class HasIdentityConverterDecorator : JsonConverter
+    {
+        private readonly JsonConverter inner;
+        private readonly IRestRequestScopeAccessor restRequestScopeAccessor;
+
+        public HasIdentityConverterDecorator(JsonConverter inner, IRestRequestScopeAccessor restRequestScopeAccessor)
+        {
+            this.inner = inner;
+            this.restRequestScopeAccessor = restRequestScopeAccessor;
+        }
+        public override bool CanRead => inner?.CanRead ?? true;
+        public override bool CanWrite => inner?.CanWrite ?? true;
+
+        public override bool CanConvert(Type objectType) => !(inner?.CanConvert(objectType) == false);
+
+        public override object ReadJson(JsonReader reader, Type objectType, object existingValue, JsonSerializer serializer)
+            => inner.ReadJson(reader, objectType, existingValue, serializer);
+
+        public override void WriteJson(JsonWriter writer, object value, JsonSerializer serializer)
+        {
+            if (value is IHasIdentity hid)
+            {
+                restRequestScopeAccessor.Scope
+                    .With<SerializationContext>(ctx => ctx.Without(hid.Id))
+                    .Run(() => inner.WriteJson(writer, value, serializer));
+            }
+            else
+                inner.WriteJson(writer, value, serializer);
+        }
+    }
+}
--- Biz.Morsink.Rest.HttpConverter.Json/RestJsonContractResolver.cs
+++ Biz.Morsink.Rest.HttpConverter.Json/RestJsonContractResolver.cs
@@ -11,6 +11,10 @@ using System.Linq;
 
 namespace Biz.Morsink.Rest.HttpConverter.Json
 {
+    using Biz.Morsink.Identity.PathProvider;
+    using Biz.Morsink.Rest.Utils;
+    using Newtonsoft.Json;
+    using System.Reflection;
     using static Biz.Morsink.Rest.FSharp.Utils;
     /// <summary>
     /// A Json Contract Resolver for Rest. 
@@ -20,16 +24,18 @@ namespace Biz.Morsink.Rest.HttpConverter.Json
     {
         private readonly IJsonSchemaTranslator[] translators;
         private readonly ITypeRepresentation[] typeRepresentations;
+        private readonly IRestRequestScopeAccessor restRequestScopeAccessor;
         private readonly IOptions<JsonHttpConverterOptions> options;
 
         /// <summary>
         /// Constructor.
         /// </summary>
         /// <param name="serviceProvider">A service provider to instantiate dependencies.</param>
-        public RestJsonContractResolver(IEnumerable<IJsonSchemaTranslator> translators, IEnumerable<ITypeRepresentation> typeRepresentations, IOptions<JsonHttpConverterOptions> options)
+        public RestJsonContractResolver(IEnumerable<IJsonSchemaTranslator> translators, IEnumerable<ITypeRepresentation> typeRepresentations, IRestRequestScopeAccessor restRequestScopeAccessor, IOptions<JsonHttpConverterOptions> options)
         {
             this.translators = translators.ToArray();
             this.typeRepresentations = typeRepresentations.ToArray();
+            this.restRequestScopeAccessor = restRequestScopeAccessor;
             this.options = options;
         }
         private void Initialize()
@@ -60,7 +66,77 @@ namespace Biz.Morsink.Rest.HttpConverter.Json
                 if (FSharp.FSharpOptionConverter.IsFSharpOptionType(objectType))
                     contract.Converter = new FSharp.FSharpOptionConverter(objectType);
             }
+            if (contract is JsonObjectContract objContract)
+            {
+                contract.Converter = contract.Converter
+                    ?? new DefaultJsonConverter(objContract, objectType, this.CreateProperties(objectType, MemberSerialization.OptOut));
+                if (typeof(IHasIdentity).IsAssignableFrom(objectType))
+                    contract.Converter = new HasIdentityConverterDecorator(contract.Converter, restRequestScopeAccessor);
+            }
             return contract;
+        }
+        private class DefaultJsonConverter : JsonConverter
+        {
+            private readonly IList<JsonProperty> properties;
+            private readonly Dictionary<string, JsonProperty> indexedProperties;
+            private readonly JsonObjectContract contract;
+            private readonly Type type;
+
+            public DefaultJsonConverter(JsonObjectContract contract, Type type, IList<JsonProperty> properties)
+            {
+                this.contract = contract;
+                this.type = type;
+                this.properties = properties;
+                indexedProperties = properties.ToDictionary(p => p.PropertyName, CaseInsensitiveEqualityComparer.Instance);
+            }
+
+            public override bool CanConvert(Type objectType)
+                => type == objectType;
+
+            public override object ReadJson(JsonReader reader, Type objectType, object existingValue, JsonSerializer serializer)
+            {
+                if (reader.TokenType == JsonToken.Null)
+                    return null;
+                if (reader.TokenType != JsonToken.StartObject)
+                    throw new JsonSerializationException();
+                reader.Read();
+                var res = contract.DefaultCreator();
+                while (reader.TokenType == JsonToken.PropertyName)
+                {
+                    var prop = indexedProperties[reader.Value.ToString()];
+                    reader.Read();
+                    var val = serializer.Deserialize(reader, prop.PropertyType);
+                    prop.ValueProvider.SetValue(res, val);
+                    reader.Read();
+                }
+                if (reader.TokenType != JsonToken.EndObject)
+                    throw new JsonSerializationException();
+
+                return res;
+            }
+
+            public override void WriteJson(JsonWriter writer, object value, JsonSerializer serializer)
+            {
+                writer.WriteStartObject();
+                foreach (var prop in properties)
+                {
+                    //var pi = type.GetTypeInfo()
+                    //    .Iterate(ti => ti.BaseType.GetTypeInfo())
+                    //    .TakeWhile(ti => ti!=typeof(object))
+                    //    .SelectMany(ti => ti.DeclaredProperties)
+                    //    .Where(p => CaseInsensitiveEqualityComparer.Instance.Equals(p.Name, prop.PropertyName) && !p.GetMethod.IsStatic && p.GetMethod.IsPublic)
+                    //    .First();
+                    //.GetProperty(prop.PropertyName, BindingFlags.Instance | BindingFlags.Public | BindingFlags.IgnoreCase);
+                    var val = prop.ValueProvider.GetValue(value);
+                    if (val != null)
+                    {
+                        writer.WritePropertyName(prop.PropertyName);
+                        serializer.Serialize(writer, val);
+                        //prop.Converter.WriteJson(writer, val, serializer);
+                    }
+                }
+                writer.WriteEndObject();
+            }
         }
     }
 }
