0a1be32aba1e7a7a7910dcfd0df4eb3205c3e528
Joost W. Morsink <morsink@gmail.com>
2018-12-04 14:18:31 +0100

    Work in progress

---
+++ Biz.Morsink.Rest.AspNetCore/Caching/CacheSerializer.cs
@@ -0,0 +1,54 @@
+﻿using Biz.Morsink.DataConvert;
+using Biz.Morsink.Identity;
+using Biz.Morsink.Rest.Schema;
+using Biz.Morsink.Rest.Serialization;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Biz.Morsink.Rest.AspNetCore.Caching
+{
+    public class CacheSerializer : Serializer<Serialization.SerializationContext>
+    {
+        private readonly IRestIdentityProvider identityProvider;
+
+        public CacheSerializer(ITypeDescriptorCreator typeDescriptorCreator, IRestIdentityProvider identityProvider,IDataConverter converter = null) : base(typeDescriptorCreator, converter)
+        {
+            this.identityProvider = identityProvider;
+        }
+        protected override IForType CreateSerializer(Type ty)
+        {
+            var ser = base.CreateSerializer(ty);
+            if (typeof(IHasIdentity).IsAssignableFrom(ty))
+                return CreateHasIdentitySerializer(ser, ty);
+            else
+                return ser;
+        }
+
+        private IForType CreateHasIdentitySerializer(IForType ser, Type ty)
+            => (IForType)Activator.CreateInstance(typeof(HasIdentityType<>).MakeGenericType(ty), this, ser);
+
+        private class HasIdentityType<T> : Typed<T>
+            where T : IHasIdentity
+        {
+            private readonly Typed<T> inner;
+            public new CacheSerializer Parent => (CacheSerializer)base.Parent;
+            public HasIdentityType(CacheSerializer parent, Typed<T> inner) : base(parent)
+            {
+                this.inner = inner;
+            }
+            public override T Deserialize(Serialization.SerializationContext context, SItem item)
+            {
+                return inner.Deserialize(context, item);
+            }
+            public override SItem Serialize(Serialization.SerializationContext context, T item)
+            {
+                if (context.IsInParentChain(item.Id))
+                    return Parent.Serialize(context.WithParent(item.Id), item.Id);
+                else
+                    return inner.Serialize(context.WithParent(item.Id), item);
+            }
+        }
+
+    }
+}
---
+++ Biz.Morsink.Rest.AspNetCore/Caching/RestDistributedCache.cs
@@ -0,0 +1,66 @@
+﻿using Biz.Morsink.DataConvert;
+using Biz.Morsink.Rest.Schema;
+using Microsoft.Extensions.Caching.Distributed;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Biz.Morsink.Rest.AspNetCore.Caching
+{
+    public class RestDistributedCache : IRestCache
+    {
+        private readonly IDistributedCache cache;
+        private readonly IRestIdentityProvider identityProvider;
+        private readonly ITypeDescriptorCreator typeDescriptorCreator;
+        private readonly CacheSerializer serializer;
+
+        public RestDistributedCache(IDistributedCache cache, ITypeDescriptorCreator typeDescriptorCreator,  IRestIdentityProvider identityProvider)
+        {
+            this.cache = cache;
+            this.identityProvider = identityProvider;
+            this.typeDescriptorCreator = typeDescriptorCreator;
+            this.serializer = new CacheSerializer(typeDescriptorCreator, identityProvider);
+
+        }
+        private string GetKey(RestRequest request)
+        {
+            var converter = identityProvider.GetConverter(request.Address.ForType, false);
+            if (converter.Convert(request.Address.Value).TryTo(out string id))
+                return string.Concat(request.Address.ForType.FullName, "|", id);
+            else
+                return null;
+        }
+        private byte[] GetValue(RestResponse restResponse)
+        {
+           
+
+        }
+        public ValueTask<int> ClearCachedResult(RestRequest request)
+        {
+            var key = GetKey(request);
+            if (key == null)
+                return new ValueTask<int>(0);
+            else
+                return remove();
+            
+            async ValueTask<int> remove()
+            {
+                await cache.RemoveAsync(key);
+                return 1;
+            }
+        }
+
+        public ValueTask<CacheResult> GetCachedResult(RestRequest request)
+        {
+            throw new NotImplementedException();
+        }
+
+        public ValueTask<int> SetCachedResult(RestRequest request, RestResponse response)
+        {
+            throw new NotImplementedException();
+        }
+
+        
+    }
+}
